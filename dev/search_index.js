0290311c534c70b3978ea5895ac95afdf2e24aa1
Documenter.jl <documenter@juliadocs.github.io>
2022-04-04 16:53:41 +0000

    build based on 139a75e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NamedGraphs","category":"page"},{"location":"#NamedGraphs","page":"Home","title":"NamedGraphs","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NamedGraphs.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NamedGraphs]","category":"page"}]
+}
