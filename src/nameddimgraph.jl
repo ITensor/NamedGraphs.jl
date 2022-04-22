f8a496d53df80a7aab119f59b03eda748f63501f
Matthew Fishman <mfishman@flatironinstitute.org>
2022-04-22 10:59:55 -0400

    Define hcat, vcat, disjoint_union for AbstractGraph

--- src/nameddimgraph.jl
+++ src/nameddimgraph.jl
@@ -133,20 +133,20 @@ function hvncat(
   return NamedDimGraph(graph_parent_graph, graph_vertices)
 end
 
-function vcat(graph1::NamedDimGraph, graph2::NamedDimGraph; kwargs...)
+function vcat(graph1::AbstractGraph, graph2::AbstractGraph; kwargs...)
   return hvncat(1, graph1, graph2; kwargs...)
 end
 
-function hcat(graph1::NamedDimGraph, graph2::NamedDimGraph; kwargs...)
+function hcat(graph1::AbstractGraph, graph2::AbstractGraph; kwargs...)
   return hvncat(2, graph1, graph2; kwargs...)
 end
 
 # TODO: define `disjoint_union(graphs...; dim::Int, new_dim_names)` to do a disjoint union
 # of a number of graphs.
-function disjoint_union(graph1::NamedDimGraph, graph2::NamedDimGraph; dim::Int=0, kwargs...)
+function disjoint_union(graph1::AbstractGraph, graph2::AbstractGraph; dim::Int=0, kwargs...)
   return hvncat(dim, graph1, graph2; kwargs...)
 end
 
-function ⊔(graph1::NamedDimGraph, graph2::NamedDimGraph; kwargs...)
+function ⊔(graph1::AbstractGraph, graph2::AbstractGraph; kwargs...)
   return disjoint_union(graph1, graph2; kwargs...)
 end
