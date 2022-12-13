fc697d84d7a612e6de36d3ceeb46dc082310c3b9
mtfishman <mfishman@flatironinstitute.org>
2022-12-13 15:55:52 -0500

    Improve mincut example

--- examples/mincut.jl
+++ examples/mincut.jl
@@ -3,12 +3,19 @@ using GraphsFlows
 
 g = NamedGraph(path_graph(4), ["A", "B", "C", "D"])
 
-part1, part2, flow = GraphsFlows.mincut(g, "A", "D")
-@show part1, part2, flow
+part1, part2 = mincut_partitions(g)
+@show part1, part2
+
+part1, part2 = mincut_partitions(g, "A", "D")
+@show part1, part2
 
 weights = Dict{Any,Float64}()
 weights["A", "B"] = 3.0
 weights["B", "C"] = 2.0
 weights["C", "D"] = 3.0
-part1, part2, flow = GraphsFlows.mincut(g, "A", "D", weights)
-@show part1, part2, flow
+
+part1, part2 = mincut_partitions(g, weights)
+@show part1, part2
+
+part1, part2 = mincut_partitions(g, "A", "D", weights)
+@show part1, part2
