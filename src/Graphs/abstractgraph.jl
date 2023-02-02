9798601c5fcfff13d62f966e6cc47c8dd68a9a9a
Joseph Tindall <joeytindall94@googlemail.com>
2023-02-02 14:55:03 -0500

    Formatting and Test Layout

--- src/Graphs/abstractgraph.jl
+++ src/Graphs/abstractgraph.jl
@@ -382,32 +382,32 @@ end
 
 """Remove a list of edges from a graph g"""
 function rem_edges(g::AbstractGraph, edges)
-    g_out = copy(g)
-    for e in edges
-        rem_edge!(g_out, e)
-    end
+  g_out = copy(g)
+  for e in edges
+    rem_edge!(g_out, e)
+  end
 
-    return g_out
+  return g_out
 end
 
 function rem_edges!(g::AbstractGraph, edges)
-    for e in edges
-        rem_edge!(g, e)
-    end
+  for e in edges
+    rem_edge!(g, e)
+  end
 end
 
 """Add a list of edges to a graph g"""
 function add_edges(g::AbstractGraph, edges)
-    g_out = copy(g)
-    for e in edges
-        add_edge!(g_out, e)
-    end
+  g_out = copy(g)
+  for e in edges
+    add_edge!(g_out, e)
+  end
 
-    return g_out
+  return g_out
 end
 
 function add_edges!(g::AbstractGraph, edges)
-    for e in edges
-        add_edge!(g, e)
-    end
+  for e in edges
+    add_edge!(g, e)
+  end
 end
