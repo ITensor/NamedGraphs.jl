eb3e285edee63fad2efde69325078d6901d0e1ed
Joseph Tindall <joeytindall94@googlemail.com>
2023-03-30 13:46:35 -0400

    Added Test for Named Graph Generators

---
+++ test/test_namedgraphgenerators.jl
@@ -0,0 +1,22 @@
+using Test
+using NamedGraphs
+using NamedGraphs: hexagonal_lattice_graph
+using Graphs
+using Random
+
+@testset "Named Graph Generators" begin
+  g = hexagonal_lattice_graph(1, 1)
+
+  #Should just be 1 hexagon
+  @test is_path_graph(g)
+
+  #Check consistency with the output of hexagonal_lattice_graph(7,7) in networkx
+  g = hexagonal_lattice_graph(7, 7)
+  @test length(vertices(g)) == 126
+  @test length(edges(g)) == 174
+
+  #Check all vertices have degree 3 in the periodic case
+  g = hexagonal_lattice_graph(6, 6; periodic=true)
+  degree_dist = [length(neighbors(g, v)) for v in vertices(g)]
+  @test all(d -> d == 3, degree_dist)
+end
