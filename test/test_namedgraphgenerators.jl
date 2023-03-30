6726c38a6736e252ea0c87dc9a9d4550c21d0c25
Joseph Tindall <joeytindall94@googlemail.com>
2023-03-30 14:40:42 -0400

    Fixed typos in comments

--- test/test_namedgraphgenerators.jl
+++ test/test_namedgraphgenerators.jl
@@ -22,15 +22,15 @@ using Random
 
   g = triangular_lattice_graph(1, 1)
 
-  #Should just be 1 hexagon
+  #Should just be 1 triangle
   @test is_path_graph(g)
 
-  #Check consistency with the output of hexagonal_lattice_graph(7,7) in networkx
+  #Check consistency with the output of triangular_lattice_graph(7,7) in networkx
   g = triangular_lattice_graph(7, 7)
   @test length(vertices(g)) == 36
   @test length(edges(g)) == 84
 
-  #Check all vertices have degree 3 in the periodic case
+  #Check all vertices have degree 6 in the periodic case
   g = triangular_lattice_graph(6, 6; periodic=true)
   degree_dist = [length(neighbors(g, v)) for v in vertices(g)]
   @test all(d -> d == 6, degree_dist)
