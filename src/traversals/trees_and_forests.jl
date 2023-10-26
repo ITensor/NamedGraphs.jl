89d968cba8ec8232c29cbbc80c24e4496819e58e
Joseph Tindall <joeytindall94@googlemail.com>
2023-10-26 16:34:14 -0400

    Better algorithm specification

--- src/traversals/trees_and_forests.jl
+++ src/traversals/trees_and_forests.jl
@@ -1,33 +1,43 @@
+abstract type SpanningTreeAlgorithm end
+
+struct BFS <: SpanningTreeAlgorithm end
+struct RandomBFS <: SpanningTreeAlgorithm end
+struct DFS <: SpanningTreeAlgorithm end
+
+default_spanning_tree_alg() = BFS()
+
 default_root_vertex(g) = last(findmax(eccentricities(g)))
 
+function spanning_tree(g::AbstractNamedGraph; root_vertex=default_root_vertex(g))
+  return spanning_tree(default_spanning_tree_alg(), g; root_vertex)
+end
+
+function spanning_tree(::BFS, g::AbstractNamedGraph; root_vertex=default_root_vertex(g))
+  @assert !NamedGraphs.is_directed(g)
+  return undirected_graph(bfs_tree(g, root_vertex))
+end
+
 function spanning_tree(
-  g::AbstractNamedGraph; root_vertex=default_root_vertex(g), alg::String="BFS"
+  ::RandomBFS, g::AbstractNamedGraph; root_vertex=default_root_vertex(g)
 )
   @assert !NamedGraphs.is_directed(g)
-  if alg == "BFS"
-    return undirected_graph(bfs_tree(g, root_vertex))
-  elseif alg == "RandomBFS"
-    return undirected_graph(random_bfs_tree(g, root_vertex))
-  elseif alg == "DFS"
-    return undirected_graph(dfs_tree(g, root_vertex))
-  else
-    error("Algorithm not current supported")
-  end
+  return undirected_graph(random_bfs_tree(g, root_vertex))
+end
+
+function spanning_tree(::DFS, g::AbstractNamedGraph; root_vertex=default_root_vertex(g))
+  @assert !NamedGraphs.is_directed(g)
+  return undirected_graph(dfs_tree(g, root_vertex))
 end
 
 #Given a graph, split it into its connected components, construct a spanning tree over each of them
 # and take the union.
-function spanning_forest(
-  g::AbstractNamedGraph; spanning_tree_function=g -> spanning_tree(g)
-)
+function spanning_forest(g::AbstractNamedGraph; spanning_tree_function=spanning_tree)
   return reduce(union, (spanning_tree_function(g[vs]) for vs in connected_components(g)))
 end
 
 #Given an undirected graph g with vertex set V, build a set of forests (each with vertex set V) which covers all edges in g
 # (see https://en.wikipedia.org/wiki/Arboricity) We do not find the minimum but our tests show this algorithm performs well
-function build_forest_cover(
-  g::AbstractNamedGraph; spanning_tree_function=g -> spanning_tree(g)
-)
+function forest_cover(g::AbstractNamedGraph; spanning_tree_function=spanning_tree)
   edges_collected = edgetype(g)[]
   remaining_edges = edges(g)
   forests = NamedGraph[]
