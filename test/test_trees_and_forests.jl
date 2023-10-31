896deb7631f899df591a3cf2e3fb5a89020a366a
Joey <joeytindall94@googlemail.com>
2023-10-31 16:12:33 -0400

    Moved variables outside global scope in test_trees_and_forests.jl

--- test/test_trees_and_forests.jl
+++ test/test_trees_and_forests.jl
@@ -1,9 +1,11 @@
 using Test
 using Graphs
 using NamedGraphs
-using NamedGraphs:
-  hexagonal_lattice_graph, triangular_lattice_graph, forest_cover, spanning_tree
+using NamedGraphs: forest_cover, spanning_tree
 
+module TestTreesAndForests
+using NamedGraphs
+using NamedGraphs: hexagonal_lattice_graph, triangular_lattice_graph
 gs = [
   ("Chain", named_grid((6, 1))),
   ("Cubic Lattice", named_grid((3, 3, 3))),
@@ -11,17 +13,20 @@ gs = [
   ("Comb Tree", named_comb_tree((4, 4))),
   ("Square lattice", named_grid((10, 10))),
   ("Triangular Grid", triangular_lattice_graph(5, 5; periodic=true)),
-];
-algs = [NamedGraphs.BFS(), NamedGraphs.DFS(), NamedGraphs.RandomBFS()];
+]
+algs = [NamedGraphs.BFS(), NamedGraphs.DFS(), NamedGraphs.RandomBFS()]
+end
 
-@testset "Test Spanning Trees $g_string, $alg" for (g_string, g) in gs, alg in algs
+@testset "Test Spanning Trees $g_string, $alg" for (g_string, g) in TestTreesAndForests.gs,
+  alg in TestTreesAndForests.algs
+
   s_tree = spanning_tree(alg, g)
   @test is_tree(s_tree)
   @test Set(vertices(s_tree)) == Set(vertices(g))
   @test issubset(Set(edges(s_tree)), Set(edges(g)))
 end
 
-@testset "Test Forest Cover $g_string" for (g_string, g) in gs
+@testset "Test Forest Cover $g_string" for (g_string, g) in TestTreesAndForests.gs
   cover = forest_cover(g)
   cover_edges = reduce(vcat, edges.(cover))
   @test issetequal(cover_edges, edges(g))
