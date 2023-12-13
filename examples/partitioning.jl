e4d23ffe5da9eda99d55ba045672200583993d96
Joey <joeytindall94@googlemail.com>
2023-12-13 09:39:29 -0500

    Cleaned up partitioning example

--- examples/partitioning.jl
+++ examples/partitioning.jl
@@ -1,4 +1,4 @@
-using Graphs
+using NamedGraphs
 using KaHyPar
 using Metis
 
@@ -6,7 +6,7 @@ g = named_grid((4, 4))
 npartitions = 4
 
 pg_kahypar = PartitionedGraph(g; npartitions, backend="KaHyPar")
-pg_metis = PartitionedGraph(g; npartitions, backend="KaHyPar")
+pg_metis = PartitionedGraph(g; npartitions, backend="Metis")
 
 @show length(vertices(pg_kahypar.partitioned_graph)) ==
   length(vertices(pg_metis.partitioned_graph)) ==
