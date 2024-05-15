3c385240d64b5504ee42d1b384dd9b18f7c56896
Matt Fishman <mtfishman@users.noreply.github.com>
2024-05-15 10:44:31 -0400

    Update src/lib/GraphsExtensions/src/neighbors.jl

--- src/lib/GraphsExtensions/src/neighbors.jl
+++ src/lib/GraphsExtensions/src/neighbors.jl
@@ -1,8 +1,8 @@
 using Graphs: AbstractGraph, neighborhood_dists
 
 function vertices_at_distance(g::AbstractGraph, vertex, distance::Int)
-  n_dists = neighborhood_dists(g, vertex, distance)
-  return map(first, filter(==(distance) ∘ last, n_dists))
+  vertices_and_distances = neighborhood_dists(g, vertex, distance)
+  return map(first, filter(==(distance) ∘ last, vertices_and_distances))
 end
 
 next_nearest_neighbors(g::AbstractGraph, v) = vertices_at_distance(g, v, 2)
