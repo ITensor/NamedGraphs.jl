986acb628037a8fc21b1750e4ab06f320d9674b5
Joey <joeytindall94@googlemail.com>
2025-09-26 15:32:41 -0400

    Merge

--- src/lib/PartitionedGraphs/src/partitionsgraphview.jl
+++ src/lib/PartitionedGraphs/src/partitionsgraphview.jl
@@ -9,8 +9,10 @@ partitions_graph(g::AbstractGraph) = PartitionsGraphView(PartitionedGraph(g, [ve
 
 # Graphs.jl and NamedGraphs.jl interface overloads for `PartitionsGraphView` wrapping
 # a `PartitionedGraph`.
-function NamedGraphs.position_graph_type(type::Type{<:PartitionsGraphView{V,G}}) where {V,G<:PartitionedGraph{V}}
-    return fieldtype(fieldtype(fieldtype(type, :graph), :partitions_graph), :position_graph)
+function NamedGraphs.position_graph_type(
+  type::Type{<:PartitionsGraphView{V,G}}
+) where {V,G<:PartitionedGraph{V}}
+  return fieldtype(fieldtype(fieldtype(type, :graph), :partitions_graph), :position_graph)
 end
 for f in [
   :(Graphs.add_vertex!),
@@ -24,7 +26,9 @@ for f in [
   :(NamedGraphs.vertextype),
 ]
   @eval begin
-    function $f(g::PartitionsGraphView{V,G}, args...; kwargs...) where {V,G<:PartitionedGraph{V}}
+    function $f(
+      g::PartitionsGraphView{V,G}, args...; kwargs...
+    ) where {V,G<:PartitionedGraph{V}}
       return $f(g.graph.partitions_graph, args...; kwargs...)
     end
   end
