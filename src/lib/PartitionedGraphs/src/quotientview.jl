a2b1e841c3d026c25598a696b31f4a2df451d48f
Jack Dunham <jdunham@flatironinstitute.org>
2025-11-24 14:47:06 -0500

    [PartitionedGraphs] Some `QuotientView` fixes and improvements
    - `vertices(::QuotientView)` now directly returns the keys
    `paritioned_vertices` return value.
    - `edges(::QuotientView)` now directly returns the edges of the
    `quotient_graph` return value (to coincide with interface overloading
    priority)
    - Converting a `QuotientView` to graph now calls `quotient_graph`
    directly
    - Adding methods for return directed/undirected graph types.
    
    Fix imports
    
    Fix rebase

--- src/lib/PartitionedGraphs/src/quotientview.jl
+++ src/lib/PartitionedGraphs/src/quotientview.jl
@@ -1,5 +1,5 @@
 using Graphs: AbstractGraph, rem_vertex!, rem_edge!, vertices, edges
-using .GraphsExtensions: add_edges!
+using .GraphsExtensions: directed_graph_type, undirected_graph_type
 using ..NamedGraphs: NamedGraph, position_graph_type
 
 struct QuotientView{V, G <: AbstractGraph} <: AbstractNamedGraph{V}
@@ -11,19 +11,22 @@ Base.parent(qg::QuotientView) = qg.graph
 parent_graph_type(g::AbstractGraph) = parent_graph_type(typeof(g))
 parent_graph_type(::Type{<:QuotientView{V, G}}) where {V, G} = G
 
-function Base.copy(qv::QuotientView)
-    qg = quotient_graph_type(parent_graph_type(qv))(vertices(qv))
-    add_edges!(qg, edges(qv))
-    return qg
-end
+Base.copy(qv::QuotientView) = copy(quotient_graph(parent(qv)))
 
 NamedGraphs.edgetype(Q::Type{<:QuotientView}) = quotient_graph_edgetype(parent_graph_type(Q))
 
-Graphs.vertices(qg::QuotientView) = parent.(quotientvertices(parent(qg)))
-Graphs.edges(qg::QuotientView) = parent.(quotientedges(parent(qg)))
+Graphs.vertices(qg::QuotientView) = keys(partitioned_vertices(parent(qg)))
+Graphs.edges(qg::QuotientView) = edges(quotient_graph(parent(qg)))
 
 function NamedGraphs.position_graph_type(type::Type{<:QuotientView})
     return position_graph_type(quotient_graph_type(parent_graph_type(type)))
+end
+
+function NamedGraphs.GraphsExtensions.directed_graph_type(type::Type{<:QuotientView})
+    return directed_graph_type(quotient_graph_type(parent_graph_type(type)))
+end
+function NamedGraphs.GraphsExtensions.undirected_graph_type(type::Type{<:QuotientView})
+    return undirected_graph_type(quotient_graph_type(parent_graph_type(type)))
 end
 
 function Graphs.rem_vertex!(qg::QuotientView, v)
